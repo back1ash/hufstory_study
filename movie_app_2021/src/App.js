321c9899de5dbe1dfc7dca032fda84e4b33ed71c
Back1ash <53644611+Back1ash@users.noreply.github.com>
2021-05-11 18:31:18 +0900

    2.1

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Potato from "./Potato";
+
+function Parts({props}) {
+  return <h1>Computer has {props}</h1>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>wow!</h1>
-      <Potato />
+      <Parts props="CPU" />
+      <Parts props="GPU" />
+      <Parts props="SSD" />
     </div>
     
   );
