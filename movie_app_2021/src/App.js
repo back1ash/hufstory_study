9336d1ac709b71997f560b889ac8c6c92a0f1eca
Back1ash <53644611+Back1ash@users.noreply.github.com>
2021-05-18 19:56:08 +0900

    ReactJS로 영화 웹 서비스 만들기 종료

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 
@@ -10,7 +11,9 @@ function App() {
     <HashRouter>
       <Navigation />
       <Route path="/" exact={true} component={Home} />
-      <Route path="/about" exact={true}  component={About} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
